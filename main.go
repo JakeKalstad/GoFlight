6836b7f1b12f9b7a691eac8496280be0f4d66e3f
Jake Kalstad <Jakekalstad@gmail.com>
2016-04-24 19:33:11 -0400

    gofmt...

--- main.go
+++ main.go
@@ -29,17 +29,17 @@ type RequestSlice struct {
 }
 
 type Request struct {
-	Passengers  Passengers `json:passengers`
-	Slice       []RequestSlice    `json:slice`
-	MaxPrice    string     `json:maxPrice`
-	SaleCountry string     `json:saleCountry`
-	Refundable  string     `json:refundable`
+	Passengers  Passengers     `json:passengers`
+	Slice       []RequestSlice `json:slice`
+	MaxPrice    string         `json:maxPrice`
+	SaleCountry string         `json:saleCountry`
+	Refundable  string         `json:refundable`
 }
 
 type City struct {
-	Kind string `json:kind`
-	Code string `json:code`
-	Name string `json:name`
+	Kind    string `json:kind`
+	Code    string `json:code`
+	Name    string `json:name`
 	Country string `json:country`
 }
 
@@ -51,13 +51,13 @@ type Airport struct {
 }
 
 type Tax struct {
-	Kind string `json:kind`
-	Name string `json:name`
-	ID string `json:id`
+	Kind       string `json:kind`
+	Name       string `json:name`
+	ID         string `json:id`
 	ChargeType string `json:chargeType`
-	Code string `json:code`
-	Country string `json:country`
-	SalePrice string `json:salePrice`
+	Code       string `json:code`
+	Country    string `json:country`
+	SalePrice  string `json:salePrice`
 }
 
 type Aircraft struct {
@@ -73,123 +73,122 @@ type Carrier struct {
 }
 
 type TripData struct {
-	Kind string `json:kind`
-	Airport Airport `json:airport`
-	City City `json:city`
+	Kind     string   `json:kind`
+	Airport  Airport  `json:airport`
+	City     City     `json:city`
 	Aircraft Aircraft `json:aircraft`
-	Tax Tax `json:tax`
-	Carrier Carrier `json:carrier`
+	Tax      Tax      `json:tax`
+	Carrier  Carrier  `json:carrier`
 }
 
 type TripOption struct {
-	Kind string `json:kind`
-	ID string `json:id`
+	Kind      string `json:kind`
+	ID        string `json:id`
 	SaleTotal string `json:saleTotal`
-
 }
-type Fare  struct {
-	Kind string `json:kind`
-	ID string `json:id`
-	Carrier Carrier `json:carrier`
-	Origin string `json:origin`
-	Destination string `json:destination`
-	BasisCode string `json:basisCode`
-	Private bool `json:private`
+type Fare struct {
+	Kind        string  `json:kind`
+	ID          string  `json:id`
+	Carrier     Carrier `json:carrier`
+	Origin      string  `json:origin`
+	Destination string  `json:destination`
+	BasisCode   string  `json:basisCode`
+	Private     bool    `json:private`
 }
 
 type Price struct {
-	Kind string `json:kind`
-	Fares Fare `json:fare`
+	Kind  string `json:kind`
+	Fares Fare   `json:fare`
 }
 
 type BagDescriptor struct {
-	Kind string `json:kind`
-	CommercialName string `json:commercialName`
-	Count int `json:count`
-	Description []string `json:description`
-	Subcode string `json:subcode`
+	Kind           string   `json:kind`
+	CommercialName string   `json:commercialName`
+	Count          int      `json:count`
+	Description    []string `json:description`
+	Subcode        string   `json:subcode`
 }
 
 type FreeBaggageOption struct {
-	Kind string `json:kind`
+	Kind       string        `json:kind`
 	Descriptor BagDescriptor `json:bagDescriptor`
 }
 
 type SegmentPrice struct {
-	Kind string `json:kind`
-	FareID string `json:fareId`
-	SegmentID string `json:segmentId`
+	Kind               string              `json:kind`
+	FareID             string              `json:fareId`
+	SegmentID          string              `json:segmentId`
 	FreeBaggageOptions []FreeBaggageOption `json:freeBaggageOption`
-	Kilos int `json:kilos`
-	KilosPerPiece int `json:kilosPerPiece`
-	Pieces int `json:pieces`
-	Pounds int `json:pounds`
+	Kilos              int                 `json:kilos`
+	KilosPerPiece      int                 `json:kilosPerPiece`
+	Pieces             int                 `json:pieces`
+	Pounds             int                 `json:pounds`
 }
 
 type Trips struct {
-	Kind string `json:kind`
-	RequestId string `json:requestId`
-	Data TripData `json:data`
-	Options TripOption `json:tripOption`
-	Pricing []Price `json:pricing`
-	SegmentPricing []SegmentPrice
-	BaseFareTotal string `json:baseFareTotal`
-  SaleFareTotal string `json:saleFareTotal`
-  SaleTaxTotal string `json:saleTaxTotal`
-	SaleTotal string `json:saleTotal`
-	Passengers Passengers `json:passengers`
-	Tax Tax `json:tax`
-	FareCalculation string `json:fareCalculation`
-	LatestTicketingTime string `json:latestTicketingTime`
-	PTC string `json:ptc`
-	Refundable bool `json:refundable`
+	Kind                string     `json:kind`
+	RequestId           string     `json:requestId`
+	Data                TripData   `json:data`
+	Options             TripOption `json:tripOption`
+	Pricing             []Price    `json:pricing`
+	SegmentPricing      []SegmentPrice
+	BaseFareTotal       string     `json:baseFareTotal`
+	SaleFareTotal       string     `json:saleFareTotal`
+	SaleTaxTotal        string     `json:saleTaxTotal`
+	SaleTotal           string     `json:saleTotal`
+	Passengers          Passengers `json:passengers`
+	Tax                 Tax        `json:tax`
+	FareCalculation     string     `json:fareCalculation`
+	LatestTicketingTime string     `json:latestTicketingTime`
+	PTC                 string     `json:ptc`
+	Refundable          bool       `json:refundable`
 }
 
 type Flight struct {
 	Carrier string `json:carrier`
-	Number string `json:number`
+	Number  string `json:number`
 }
 
 type Leg struct {
-	Kind string `json:kind`
-	Duration int `json:duration`
-	ID string `json:id`
-	ArrivalTime string `json:arrivalTime`
-	DepartureTime string `json:departureTime`
-	Origin string `json:origin`
-	Destination string `json:destination`
-	OriginTerminal string `json:originTerminal`
+	Kind                string `json:kind`
+	Duration            int    `json:duration`
+	ID                  string `json:id`
+	ArrivalTime         string `json:arrivalTime`
+	DepartureTime       string `json:departureTime`
+	Origin              string `json:origin`
+	Destination         string `json:destination`
+	OriginTerminal      string `json:originTerminal`
 	DestinationTerminal string `json:destinationTerminal`
 	OperatingDisclosure string `json:operatingDisclosure`
-	OnTimePerformance int `json:onTimePerformance`
-	Mileage int `json:mileage`
-	Meal string `json:meal`
-	Secure bool `json:secure`
-	ConnectionDuration int `json:connectionDuration`
-	ChangePlane bool `json:changePlane`
+	OnTimePerformance   int    `json:onTimePerformance`
+	Mileage             int    `json:mileage`
+	Meal                string `json:meal`
+	Secure              bool   `json:secure`
+	ConnectionDuration  int    `json:connectionDuration`
+	ChangePlane         bool   `json:changePlane`
 }
 
 type Segment struct {
-	Kind string `json:kind`
-	Duration string `json:duration`
-	Flight Flight `json:flight`
-	ID string `json:id`
-	Cabin string `json:cabin`
-	BookingCode string `json:bookingCode`
-	BookingCodeCount int `json:bookingCodeCount`
-	MarriedSegmentGroup string `json:marriedSegmentGroup`
-	SubjectToGovernmentApproval bool `json:subjectToGovernmentApproval`
-	Legs []Leg `json:leg`
-	ConnectionDuration int `json:connectionDuration`
+	Kind                        string `json:kind`
+	Duration                    string `json:duration`
+	Flight                      Flight `json:flight`
+	ID                          string `json:id`
+	Cabin                       string `json:cabin`
+	BookingCode                 string `json:bookingCode`
+	BookingCodeCount            int    `json:bookingCodeCount`
+	MarriedSegmentGroup         string `json:marriedSegmentGroup`
+	SubjectToGovernmentApproval bool   `json:subjectToGovernmentApproval`
+	Legs                        []Leg  `json:leg`
+	ConnectionDuration          int    `json:connectionDuration`
 }
 type ResponseSlice struct {
-	Kind  string `json:kind`
-	Duration string `json:duration`
-	Segment	[]Segment `segment`
+	Kind     string    `json:kind`
+	Duration string    `json:duration`
+	Segment  []Segment `segment`
 }
 type Response struct {
-	Kind  string `json:kind`
-	Trips Trips  `json:trips`
+	Kind  string          `json:kind`
+	Trips Trips           `json:trips`
 	Slice []ResponseSlice `json:slice`
 }
 
